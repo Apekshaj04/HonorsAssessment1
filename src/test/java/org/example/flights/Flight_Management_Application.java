c6261d2b334c80b3ef93fd97464211c872be0568
Apekshaj04 <apekshajamjar4@gmail.com>
2025-05-16 20:33:01 +0530

    Updated the project structure

---
+++ src/test/java/org/example/flights/Flight_Management_Application.java
@@ -0,0 +1,14 @@
+package org.example.flights;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Flight_Management_Application {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
