1aad7f67b625642e1d16036e81858d604091cbed
Apeksha Jamjar <117656894+Apekshaj04@users.noreply.github.com>
2025-05-17 10:35:14 +0530

    Update Flight_Management_Application.java

--- src/test/java/org/example/flights/Flight_Management_Application.java
+++ src/test/java/org/example/flights/Flight_Management_Application.java
@@ -1,14 +1,28 @@
 package org.example.flights;
 
-
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest
 class Flight_Management_Application {
 
-    @Test
-    void contextLoads() {
+    static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:15")
+            .withDatabaseName("flight_management_eldd")
+            .withUsername("flight_management_eldd_user")
+            .withPassword("password");
+
+    @BeforeAll
+    static void setUp() {
+        postgresContainer.start();
+        System.setProperty("spring.datasource.url", postgresContainer.getJdbcUrl());
+        System.setProperty("spring.datasource.username", postgresContainer.getUsername());
+        System.setProperty("spring.datasource.password", postgresContainer.getPassword());
     }
 
+    @Test
+    void contextLoads() {
+        // This test will now run with Postgres container DB
+    }
 }
