c74f3cf66541e8ed6861283a574ecd7df4a1fe39
Apeksha Jamjar <117656894+Apekshaj04@users.noreply.github.com>
2025-05-17 10:34:19 +0530

    Update DemoApplicationTests.java

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,29 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    // Define the PostgreSQL container
+    static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:15")
+        .withDatabaseName("flight_management_eldd")
+        .withUsername("flight_management_eldd_user")
+        .withPassword("password");  
 
+    @BeforeAll
+    static void setUp() {
+        postgresContainer.start();
+
+        System.setProperty("spring.datasource.url", postgresContainer.getJdbcUrl());
+        System.setProperty("spring.datasource.username", postgresContainer.getUsername());
+        System.setProperty("spring.datasource.password", postgresContainer.getPassword());
+    }
+
+    @Test
+    void contextLoads() {
+    }
 }
